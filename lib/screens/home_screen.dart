667f046cc355143da88b4663b4cfe55983a69d10
ChauDz <buichau40@gmail.com>
2023-12-09 12:49:58 +0700

    up

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/themes/app_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +9,99 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Homeeee'),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 40, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.all(Radius.circular(999)),
+                      ),
+                      child: Text("D", style: AppFont.primaryFont.copyWith(
+                          color: Colors.white
+                      ),),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('D', style: AppFont.primaryFont.copyWith(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600
+                        ),),
+                        Text("1 ngày", style: AppFont.primaryFont.copyWith(
+                          color: Colors.black12,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600
+                        ),)
+                      ],
+                    )
+                  ],
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                  ),
+                  child: Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/icons8-no-reminders-50.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                )
+
+              ],
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex){
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Trang chủ',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: "Thống kê",
+            icon: Icon(Icons.pivot_table_chart),
+          ),
+          BottomNavigationBarItem(
+            label: "Tổng quan",
+            icon: Image(image: AssetImage('assets/images/girl-icon.png'),width: 20, height: 20,),
+          ),
+        ],
+        showSelectedLabels: true, // Ẩn label khi được chọn
+        showUnselectedLabels: false, // Ẩn label khi không được chọn
+      ),
     );
   }
 }
